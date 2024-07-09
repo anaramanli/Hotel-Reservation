c1d01f493aa3226d291a2e6879426e78923a643d
anaramanli <amanlianar@gmail.com>
2024-07-09 13:30:07 +0400

    bacpac file added

--- Hotel/Hotel/obj/Debug/net8.0/Hotel.AssemblyInfo.cs
+++ Hotel/Hotel/obj/Debug/net8.0/Hotel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hotel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+604cafcadb9d60ed673c4bab13425ffea01b40c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+277356f2998a9d647eed08fe766adab61014d961")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hotel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hotel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
